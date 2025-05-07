d89fea9cc67afb2761fdc55886321a658388d7a8
In5perat0r <schlacht.mike@gmail.com>
2025-05-07 14:28:03 +0200

    Change Date: 05/07/2025 - 14:28

---
+++ Searchbar Background.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Searchbar Background
+// @namespace    http://tampermonkey.net/
+// @version      2025-05-07
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.google.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var url = new URL(document.location.href);
+    // Your code here...
+})();
