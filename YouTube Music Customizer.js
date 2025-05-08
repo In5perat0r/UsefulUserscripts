0d2f7005d227ce4d25996697a827c2636bf4f511
In5perat0r <schlacht.mike@gmail.com>
2025-05-08 12:25:09 +0200

    Change Date: 05/08/2025 - 12:25

--- YouTube Music Customizer.js
+++ YouTube Music Customizer.js
@@ -7,6 +7,8 @@
 // @match        https://music.youtube.com/watch?v=yUZeZNxiD-M&list=PLuVt-kUON6LguiFNkpP_yo666cbuWUP6q
 // @match        https://music.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=music.youtube.com
+// @updateURL    https://raw.githubusercontent.com/In5perat0r/UsefulUserscripts/refs/heads/main/YouTube%20Music%20Customizer.js
+// @downloadURL  https://raw.githubusercontent.com/In5perat0r/UsefulUserscripts/refs/heads/main/YouTube%20Music%20Customizer.js
 // @grant        GM_addStyle
 // ==/UserScript==
 
